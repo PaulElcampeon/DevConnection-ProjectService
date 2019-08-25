b45708d85d967af98ae12098c569cd7ab9178de5
PaulElcampeon <pauloladele@live.co.uk>
2019-08-25 22:55:25 +0100

    started writing tests for controller

--- src/test/java/com/devconnection/ProjectService/ProfileServiceControllerTest.java
+++ src/test/java/com/devconnection/ProjectService/ProfileServiceControllerTest.java
@@ -1,4 +1,21 @@
 package com.devconnection.ProjectService;
 
-public class ProfileServiceController {
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(controllers = {ProfileServiceController.class})
+public class ProfileServiceControllerTest {
+
+//    @Autowired
+//    private MockMvc mockMvc;
+
+
+
+
+
 }
